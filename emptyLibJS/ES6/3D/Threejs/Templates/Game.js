fc06807046eb1441e31f6667b2dd3fbf7c2477c8
Rolando Garro <rolando@emptyart.xyz>
2019-12-27 23:40:47 +0100

    belling coins click, light changing hex positions, samsung galaxy

---
+++ emptyLibJS/ES6/3D/Threejs/Templates/Game.js
@@ -0,0 +1,27 @@
+/**
+ * Threejs es6 Game Template
+ *
+ * @author <rgarro@gmail.com>
+ */
+class Game {
+  constructor() {
+    this.container = null;
+    this.renderer = null;
+    this.scene = null;
+    this.camera = null;
+    this.ini_camera_x = 15;
+    this.ini_camera_y = 16;
+    this.ini_camera_z = 13;
+    this.is_camera_set = false;
+    this.cameraControl = null;
+    this.show_control_gui = false;
+    this.show_stats = false;
+    this.ds = null; //datastats object
+    this.clearColor = 0x000000;
+    this.enable_shadows = true;
+    this.l = new Light();
+    if (typeof arguments[0] != "undefined") {
+      this.setContainer(arguments[0]);
+    }
+  }
+}
