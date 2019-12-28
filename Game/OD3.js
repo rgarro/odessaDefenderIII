ea3841e7f0c7e78e1f2bb48f6bdf3705ad3b5f21
Rolando Garro <rolando@emptyart.xyz>
2019-12-28 21:05:36 +0100

    kingston usb, all mighty loving the sky, beeping greenlights time ...
    kingston usb, all mighty loving the sky, beeping greenlights time ...

--- Game/OD3.js
+++ Game/OD3.js
@@ -1,6 +1,12 @@
 class OD3 extends Game {
   constructor() {
     super();
+    this.container = "gameContainer"; //el Honda de fabio esta detras del evergreen por la carcel de san luis ..
     console.log("here ...");
+  }
+
+  init() {
+    super.init();
+    console.log(this.scene);
   }
 }
