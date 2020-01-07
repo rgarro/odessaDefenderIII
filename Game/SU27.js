2415b8d5091c9977d19556903b51f9ee3a874ef7
Rolando Garro <rolando@emptyart.xyz>
2020-01-07 22:35:49 +0100

    starting a branching, deep blue sky cuckos beeping, all migthy jamming
    starting a branching, deep blue sky cuckos beeping, all migthy jamming

---
+++ Game/SU27.js
@@ -0,0 +1,13 @@
+class SU27 extends ControllableModel {
+  constructor() {
+    super();
+    this.vehicleMeshName = "laChilindrinaDeApaikan";
+    this.modelUrl = "/Game/assets/models/SU25/SU-25.json"; //custom blended howitzer styled turret
+    this.pixelsPerSecond = 25;
+    this.vehicleMesh = null;
+    //this.scale = 13;
+    this.vehicleColor = 0x0ffa65;
+    this.rotationAngleStep = 6;
+    this.group = new THREE.Object3D();
+  }
+}
