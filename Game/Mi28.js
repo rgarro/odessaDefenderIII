348a94ef40bea8f1d4ca13b38c0739628b7757b7
Rolando Garro <rolando@emptyart.xyz>
2020-01-23 13:18:51 -0600

    Я ем гуаву ..
    Я ем гуаву ..

--- Game/Mi28.js
+++ Game/Mi28.js
@@ -1,33 +1,37 @@
-class Mi28 extends LineWalkerModel {
+class Mi28 extends ControllableModel {
   constructor() {
     super();
-    this.modelUrl = "/emptyLibJS/3D/Games/Kalero/assets/mi28/Mi28.json";
-    this.textureUrl = "/emptyLibJS/3D/Games/Kalero/assets/mi28/Mi28NA.png";
-    this.game = null;
-    this.geometry = null;
-    this.mesh = null;
-    this.gameIsSet = false;
-    this.meshName = "helicopteroBody";
-    this.centerMeshName = "elTanque";
-    this.radiusLength = 100;
-    this.altitude = 90; //y
-    this.origin = { x: 0, y: 0, z: 0 };
-    this.angle = 30;
-    this.speed = 0.9;
-    this.clockWise = true;
-    this.modelLoaded = false;
-    this.scale = 11;
-    this.propeller = null;
-    this.rudder = null;
-    this.dropKey = "y"; //testing Wo FallingBouncer
+    this.vehicleMeshName = "laChilindrinaDeApaikan";
+
+    this.FbxModelUrl = "/Game/assets/models/SU25/SU-25.fbx";
+    this.modelUrl = "/Game/assets/models/Mi28.json";
+    this.textureUrl = "/Game/assets/models/Mi28NA.png";
+    this.ObjModelUrl = "/Game/assets/models/SU25/SU-25.obj";
+    this.pixelsPerSecond = 25;
+    this.vehicleMesh = null;
+    //this.scale = 13;
+    this.altitude = 145;
+    this.ini_x = 0;
+    //this.ini_y = 150;
+    this.ini_z = 350;
+    this.init_rotation = 160;
+    this.modelColor = 0x0d9a927;
+    this.vehicleColor = 0x0ffa65;
+    this.rotationAngleStep = 6;
     this.group = new THREE.Object3D();
-    this.ball_fell = false;
-    this.balls = [];
-    this.PropsRemover = null;
-    createjs.Sound.registerSound(
-      "/mp3/Helicopt-Diode111-8858_hifi.mp3",
-      "heliSound"
-    );
-    //createjs.Sound.registerSound("/Game/assets/sounds/Helicopt-Diode111-8858_hifi.mp3", 'heliSound');
+  }
+
+  postLoaded() {
+    this.mesh.rotation.y = this.init_rotation;
+    this.mesh.position.y = this.altitude;
+    this.mesh.position.x = this.ini_x;
+    this.mesh.position.z = this.ini_z;
+  }
+
+  initPropeller() {
+    /*this.propeller = new HeliPropeller();
+    this.propeller.origin.y = this.altitude + 10;
+    this.propeller.setGame(this.game);
+    this.propeller.loadModel("/cube/");*/
   }
 }
